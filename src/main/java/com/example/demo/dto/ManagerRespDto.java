d40f078426a877b156aa7dba03587a8ceef0b347
gaoyuebin <gaoyuebin@quyiyuan.com>
2018-01-30 18:24:09 +0800

    增加管人员页面的后台逻辑

---
+++ src/main/java/com/example/demo/dto/ManagerRespDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+/**
+ * @author 高跃斌
+ * @description
+ * @create 2018-01-29 17:39
+ **/
+@Data
+public class ManagerRespDto {
+    String name;
+    int age;
+    int sex;
+    String address;
+    String company;
+    String postcode;
+    String birth;
+}
